e24972520c4ccf2e8d79f911b6d9dcc51d9bd0dd
nhikiu <dongocnhi0110@gmail.com>
2023-04-07 17:51:59 +0700

    Future check user is exist and create user

--- lib/api/apis.dart
+++ lib/api/apis.dart
@@ -1,7 +1,37 @@
+import 'package:chat_together/models/chat_user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class APIs {
   static FirebaseAuth auth = FirebaseAuth.instance;
+
   static FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  static User get user => auth.currentUser!;
+
+  static Future<bool> userExist() async {
+    return (await firestore
+            .collection('users')
+            .doc(auth.currentUser!.uid)
+            .get())
+        .exists;
+  }
+
+  static Future<void> createUser() async {
+    final time = DateTime.now().millisecondsSinceEpoch.toString();
+
+    final chatUser = ChatUser(
+        isOnline: false,
+        id: user.uid,
+        pushToken: '',
+        imageUrl: user.photoURL.toString(),
+        email: user.email.toString(),
+        username: user.displayName.toString(),
+        lastActive: time);
+
+    return await firestore
+        .collection('users')
+        .doc(user.uid)
+        .set(chatUser.toJson());
+  }
 }
