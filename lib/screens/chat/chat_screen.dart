e7ac7a7c03ba49cbafc51ef8cf19fd668f47d983
nhikiu <dongocnhi0110@gmail.com>
2023-04-12 23:37:44 +0700

    Navigator to Profile Screen View Only

--- lib/screens/chat/chat_screen.dart
+++ lib/screens/chat/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'package:chat_together/screens/profile/profile_screen_view_only.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../models/chat_user.dart';
@@ -15,44 +17,45 @@ class ChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: InkWell(
-          onTap: () {},
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              CircleAvatar(
-                radius: 20,
-                backgroundColor: Theme.of(context).colorScheme.secondary,
-                backgroundImage: chatuser.imageUrl.isEmpty
-                    ? null
-                    : NetworkImage(chatuser.imageUrl),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    chatuser.username,
-                    maxLines: 1,
-                    style: const TextStyle(
-                        fontSize: 16,
-                        color: Colors.black54,
-                        fontWeight: FontWeight.w500),
-                  ),
-                  const Text(
-                    'Last seen not available',
-                    maxLines: 1,
-                    style: TextStyle(
-                        fontSize: 13,
-                        color: Colors.black54,
-                        fontWeight: FontWeight.w400),
-                  ),
-                ],
-              )
-            ],
-          ),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) =>
+                        ProfileScreenViewOnly(chatuser: chatuser)));
+              },
+              icon: Icon(
+                CupertinoIcons.info,
+                color: Theme.of(context).colorScheme.primary,
+              ))
+        ],
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            CircleAvatar(
+              radius: 20,
+              backgroundColor: Theme.of(context).colorScheme.secondary,
+              backgroundImage: chatuser.imageUrl.isEmpty
+                  ? null
+                  : NetworkImage(chatuser.imageUrl),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  chatuser.username,
+                  maxLines: 1,
+                  style: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.black54,
+                      fontWeight: FontWeight.w500),
+                ),
+              ],
+            )
+          ],
         ),
       ),
       body: Column(
