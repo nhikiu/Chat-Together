c541db4231708639bc1e3b6f8a5ef730b89dd846
nhikiu <dongocnhi0110@gmail.com>
2023-04-15 17:01:57 +0700

    Validator data

---
+++ lib/utils/validators.dart
@@ -0,0 +1,33 @@
+class Validators {
+  static String? validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Email is required';
+    }
+    final emailRegex =
+        RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+    if (!emailRegex.hasMatch(value)) {
+      return 'Please enter a valid email address.';
+    }
+    return null;
+  }
+
+  static String? validateUsername(String? username) {
+    if (username == null || username.isEmpty) {
+      return 'Username is required';
+    }
+    if (username.length > 12) {
+      return 'Username can be upto a maximum of 12 characters';
+    }
+    return null;
+  }
+
+  static String? validatePassword(String? password) {
+    if (password == null || password.isEmpty) {
+      return 'Password is required';
+    }
+    if (password.length < 8 || password.length > 20) {
+      return 'Password must be between 8 - 20 characters';
+    }
+    return null;
+  }
+}
